ad8f9780340d054ba27179129319212146261e56
sjh1108 <ch6854@naver.com>
2024-10-21 01:06:40 +0900

    Ch03_answer

--- Main.java
+++ Main.java
@@ -1,33 +1,48 @@
 /* 
-    * 2차원 평면에서 두 점으로 이루어진 직사각형 r이 있다. (Ch02_Rect.java 참조)
-    * 1. 키보드로부터 점의 좌표를 입력받아 
-    * 2. 직사각형 r 내부에 존재하는지, 
-    * 3. 직사각형의 선 위에 존재하는지 판별하는 프로그램을 작성하라
+    * 0부터 100까지 숫자를 저장하는 배열을 선언하고,
+    * 키보드로 부터 입력을 받아 해당 숫자가 소수인지 판별하는 프로그램을 작성하시오
  */
 
 //TODO#1: Import Scanenr class
+import java.util.Scanner;
 
 public class Main{
-    public static Ch02_Rect r = new Ch02_Rect();
     public static void main(String[] args){
-        //TODO#2: Create a Scanner object
+        //TODO#2: Scanner 객체 생성
+        Scanner scanner = new Scanner(System.in);
 
-        //TODO#3: Read two integers from the keyboard
+        //TODO#3: 0부터 100까지 숫자를 저장하는 배열 선언
+        int[] numbers = new int[101];
+        //TODO#4: 사용자로부터 입력을 받아 해당 숫자가 소수인지 판별
+        System.out.println("0부터 100까지의 숫자를 입력하세요: ");
+        int input = scanner.nextInt();
 
-        //TODO#6 Call inRect, onLine method
+        //TODO#6: 소수인지 판별하는 메소드 호출 및 결과 출력
+        boolean isPrime = isPrime(numbers, input);
+        if(isPrime){
+            System.out.println(input + "은 소수입니다.");
+        }else{
+            System.out.println(input + "은 소수가 아닙니다.");
+        }
 
-        //TODO#7: Print the result
-
-        //TODO#8: Close the Scanner
+        //TODO#7: Scanner 객체 close
+        scanner.close();
     }
 
-    // TODO#4: Implement inRect method
-    public static boolean inRect(int x, int y){
-        // fill the method
-    }
+    //TODO#5: 소수인지 판별하는 메소드 구현
+    public static boolean isPrime(int[] numbers, int input){
+        //TODO#5-1: 소수인지 판별하는 로직 구현
+        if(input < 2){
+            return false;
+        }
 
-    // TODO#5: Implement onLine method
-    public static boolean onLine(int x, int y){
-        // fill the method
+        boolean flag = true;
+        for(int i = 2; i <= Math.sqrt(input); i++){
+            if(input % i == 0){
+                flag = false;
+            }
+        }
+
+        return flag;
     }
 }
